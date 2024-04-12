948af4ff2d263805c8e8cf7cfb15966f66d94514
labanca <moraislabanca@gmail.com>
2024-04-12 14:56:12 -0300

    pipeline completa

--- scripts/temp.py
+++
@@ -1,6 +0,0 @@
-from frictionless.resources import TableResource
-
-
-resource = TableResource("data-raw/Programa_monitoramento.txt", dialect="dialect.yaml", format='csv')
-resource.infer()
-print(resource.schema.to_yaml("schemas/programa_monitoramento.yaml"))
